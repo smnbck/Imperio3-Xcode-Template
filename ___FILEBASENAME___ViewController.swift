4bb7f48de91abd39663531be3035aab5cf71603b
Simon Back <simon.back@jamitlabs.com>
2019-08-20 11:12:21 +0200

    Add missing marks

--- ___FILEBASENAME___ViewController.swift
+++ ___FILEBASENAME___ViewController.swift
@@ -3,13 +3,16 @@
 import UIKit
 
 class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+    // MARK: - Properties
     weak var flowDelegate: ___VARIABLE_productName:identifier___FlowDelegate?
 
+    // MARK: - View Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
     }
 
+    // MARK: - Instance Methods
     private func setupView() {
     }
 }
